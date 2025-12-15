0d07b914c82feb12580c4de6ad1e0be045933999
Dominic <schmitzdominic@gmail.com>
2025-12-15 11:48:03 +0100

    home component optimized

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,90 +1,116 @@
-import { Component, inject, Injector, runInInjectionContext, ViewChild } from '@angular/core';
+import { Component, inject, Injectable, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import {DateService} from "../../services/date/date.service";
 import {MenuTitleService} from "../../../shared/behavior/menu-title/menu-title.service";
 import {BudgetService} from "../../services/budget/budget.service";
 import {Budget} from "../../../shared/interfaces/budget.model";
 import {NgbModalRef} from "@ng-bootstrap/ng-bootstrap";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
-import {AccountType} from "../../../shared/enums/account-type.enum";
 import {PredictService} from "../../services/predict/predict.service";
-import { map, takeUntil } from 'rxjs/operators';
+import { forkJoin, map, Subject, take, takeUntil } from 'rxjs';
 
+/**
+ * HomeComponent displays the main dashboard, showing budgets for the current month and all-time budgets.
+ */
+@Injectable()
 @Component({
     selector: 'app-actual-month',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
     standalone: false
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   private menuTitleService = inject(MenuTitleService);
   private dateService = inject(DateService);
   private budgetService = inject(BudgetService);
   private ngbModal = inject(NgbModal);
   private predictService = inject(PredictService);
-  private injector = inject(Injector);
 
+  // Reference to the <ng-template> in the HTML
+  @ViewChild('viewBudgetModal') private viewBudgetModalTemplate: TemplateRef<any> | undefined;
 
-  @ViewChild('viewBudgetModal') viewBudgetModal: NgbModalRef | undefined;
+  private activeModalRef: NgbModalRef | undefined;
 
-  protected readonly AccountType = AccountType;
+  public actualMonth: string = this.dateService.getActualMonthName();
+  public monthString: string = this.dateService.getActualMonthString();
+  public actualYear: number = this.dateService.getActualYear();
 
-  viewBudgetModalRef: NgbModalRef | undefined;
+  public clickedBudget: Budget | undefined;
+  public allBudgets: (Budget & { id: string })[] = [];
 
-  actualMonth: string = this.dateService.getActualMonthName();
-  monthString: string = this.dateService.getActualMonthString();
+  // Subject to automatically unsubscribe from observables on component destruction
+  private destroy$ = new Subject<void>();
 
-  actualYear: number = this.dateService.getActualYear();
-
-  clickedBudget: Budget | undefined;
-
-  allTimeBudgets: (Budget & { id: string })[] = [];
-  monthlyBudgets: (Budget & { id: string })[] = [];
-
-  ngOnInit(): void {
+  /**
+   * Angular lifecycle hook that runs on component initialization.
+   */
+  public ngOnInit(): void {
     this.predictService.createEntries();
     this.setInitialValues();
-    this.loadBudgets();
+    this.loadAllBudgets();
   }
 
-  setInitialValues(): void {
+  /**
+   * Angular lifecycle hook that runs when the component is destroyed.
+   */
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  /**
+   * Sets the initial values for the component, such as the menu title.
+   */
+  private setInitialValues(): void {
     const actualDate: Date = this.dateService.getActualDate();
     this.menuTitleService.setTitle(this.dateService.getDayShortName(actualDate.getDay()) + ' ' + actualDate.getDate() + '. ' + this.actualMonth + ' ' + this.actualYear);
     this.menuTitleService.setActiveId(1);
   }
 
-  loadBudgets() {
-    this.loadAllTimeBudgets();
-    this.loadMonthlyBudgets();
-  }
+  /**
+   * Loads all-time and monthly budgets concurrently and merges them.
+   */
+  private loadAllBudgets(): void {
+    const allTimeBudgets$ = this.budgetService.getAllNoTimeLimitBudgets();
+    const monthlyBudgets$ = this.budgetService.getAllMonthBudgetsByMonthString(this.dateService.getActualMonthString());
 
-  private loadAllTimeBudgets() {
-    this.budgetService.getAllNoTimeLimitBudgets().pipe(
-      map(budgets => budgets as (Budget & { id: string })[])
-    ).subscribe(budgets => runInInjectionContext(this.injector, () => {
-      this.allTimeBudgets = budgets.filter(budget => !budget.isArchived);
-    }));
-  }
-
-  private loadMonthlyBudgets() {
-    this.budgetService.getAllMonthBudgetsByMonthString(this.dateService.getActualMonthString()).pipe(
-      map(budgets => budgets as (Budget & { id: string })[])
-    ).subscribe(budgets => {
-      this.monthlyBudgets = budgets.filter(budget => !budget.isArchived);
+    forkJoin([
+      allTimeBudgets$.pipe(take(1)),
+      monthlyBudgets$.pipe(take(1))
+    ]).pipe(
+      map(([allTime, monthly]) => {
+        const allTimeTyped = allTime as (Budget & { id: string })[];
+        const monthlyTyped = monthly as (Budget & { id: string })[];
+        // Filter out archived budgets and combine them into a single array
+        return [
+          ...allTimeTyped.filter(budget => !budget.isArchived),
+          ...monthlyTyped.filter(budget => !budget.isArchived)
+        ];
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe(combinedBudgets => {
+      this.allBudgets = combinedBudgets;
     });
   }
 
-  openViewBudgetModal(budget: Budget): void {
+  /**
+   * Opens a modal to view the details of a selected budget.
+   * @param budget The budget to be displayed in the modal.
+   */
+  public openViewBudgetModal(budget: Budget): void {
     this.clickedBudget = budget;
-    this.viewBudgetModalRef = this.ngbModal.open(
-      this.viewBudgetModal,
+    this.activeModalRef = this.ngbModal.open(
+      this.viewBudgetModalTemplate,
       {
         size: 'md'
       });
   }
 
-  onCloseViewBudgetModal(): void {
-    if (this.viewBudgetModalRef) {
-      this.viewBudgetModalRef.close();
+  /**
+   * Closes the currently open budget view modal.
+   */
+  public onCloseViewBudgetModal(): void {
+    if (this.activeModalRef) {
+      this.activeModalRef.close();
     }
   }
 }
