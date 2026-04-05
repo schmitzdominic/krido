3dfd8bbfcb402445e2d779801bdcf65a292bf0b6
Dominic Schmitz <schmitzdominic@gmail.com>
2026-04-05 18:31:27 +0200

    Remove 'Nächsten Monat generieren' button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -73,10 +73,6 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.setInitialValues();
   }
 
-  public regenerateNextMonth(): void {
-    this.predictService.forceCreateNextMonth();
-  }
-
   /**
    * Angular lifecycle hook that runs when the component is destroyed.
    */
